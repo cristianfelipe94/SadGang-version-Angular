aec46b44cf8df007da507b50283b109990ca32cf
cristianfelipecalderonfonseca <c.calderon.fonseca@accenture.com>
2020-09-28 14:03:05 -0600

    Have initialized project, added basic structure and files to start. Clear some information that comes within the CLI project.

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.sass']
+})
+export class AdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  title = 'sadgang-version-angular';
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'admin', component: AdminComponent },
+  { path: 'home',component: AppComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
