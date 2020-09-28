80ee220b9155eaf50cf390d3dc5f75f2dfbdfdf2
cristianfelipecalderonfonseca <c.calderon.fonseca@accenture.com>
2020-09-28 16:00:55 -0600

    Have adjusted rendering twice elements in template.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'admin', component: AdminComponent },
-  { path: 'home',component: AppComponent },
+  { path: 'home',component: HomeComponent },
 ];
 
 @NgModule({
