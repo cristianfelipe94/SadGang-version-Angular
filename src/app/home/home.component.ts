3880da1c7b93e0b990660c0caa502c7786401d7c
cristianfelipecalderonfonseca <c.calderon.fonseca@accenture.com>
2020-09-29 14:58:35 -0600

    Have adjusted typo with class declaration in HTML tags. Also, shared information between component.ts to layout.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  gallery= {
+    title: 'SadGang',
+    description: 'Costa Rica Streetware'
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
